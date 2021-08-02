64dc07695499ad6b53d842520e076ee1fbc0b1f6
EC2 Default User <ec2-user@ip-172-31-45-77.ec2.internal>
2021-08-02 16:44:41 +0000

    first

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+#Rails 7章　画像投稿処理不可のため小田追記(2021/8/2)
+Refile.secret_key = '010097ad498b94f4c232aa81ad35f029a7af23c18b9501065b0973114c0663cce21402382dd64102bba517ae43c7543332754451e60929c4acbf0746345c76c9'
